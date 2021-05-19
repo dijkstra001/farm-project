babd29273710635c138f09a9fdea13cc2c62ff3f
dijkstra001 <ncorreiatrabalhando@gmail.com>
2021-05-19 09:11:07 -0300

    Add queries with agg function

--- scripts/queries.sql
+++ scripts/queries.sql
@@ -4,6 +4,19 @@ select * from fabricantes;
 -- Selecionar produtos:
 select * from produtos;
 
--- selecionar produtos e fabricantes:
+-- Selecionar produtos e fabricantes:
 select p.id, p.nome, f.nome from produtos p, fabricantes f 
 where p.id_fabricante = f.id
+
+-- Quantidade de produtos pedidos:
+select p.nome as "Produto", sum(pp.quantidade) as "Quantidade"
+from produtos_pedidos pp, produtos p
+where p.id = pp.id_produto
+group by "Produto"
+order by "Quantidade" desc
+
+-- Quantidade de produtos por pedido:
+select id_pedido as "Código", sum(quantidade) as "Quantidade"
+from produtos_pedidos 
+group by "Código"
+order by "Quantidade" desc
